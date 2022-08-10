da0626a2eee3795dd5a53009e73439a571d69a5f
Leonardo Freitas <46468408+LeonardoFreitas1@users.noreply.github.com>
2022-08-10 19:34:48 -0300

    creating basic model

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let url = &args[1];
+    let response = reqwest::blocking::get(url,)
+        .unwrap()
+        .text()
+        .unwrap();
+    
+    let document = scraper::Html::parse_document(&response);
+    let title_selector = scraper::Selector::parse("title").unwrap();
+    let title = document.select(&title_selector).next().unwrap();
+    let text = title.text().collect::<Vec<_>>();
+
+    println!("title: {}", text[0]);
 }
