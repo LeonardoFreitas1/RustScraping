544b62494fa31516afa1f4d4f7aa4e542b95c12f
Leonardo Freitas <leonardo.paulino.ps@ojc.com.br>
2022-08-11 08:59:07 -0300

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use std::env;
+use reqwest::blocking;
+use scraper::{Html, Selector};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let url = &args[1];
-    let response = reqwest::blocking::get(url,)
+    let response = blocking::get(url)
         .unwrap()
         .text()
         .unwrap();
     
-    let document = scraper::Html::parse_document(&response);
-    let title_selector = scraper::Selector::parse("title").unwrap();
+    let document = Html::parse_document(&response);
+    let title_selector = Selector::parse("title").unwrap();
     let title = document.select(&title_selector).next().unwrap();
-    let text = title.text().collect::<Vec<_>>();
+    let text = title.text().collect::<Vec<_>>().join("");
 
-    println!("title: {}", text[0]);
+    println!("title: {}", text);
 }
